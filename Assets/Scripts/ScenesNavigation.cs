b0f8b9178e7ef4c30321dbc16067eaa5e680b197
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2016-03-10 10:50:44 +0200

    Navigate to Signal Flow Graph & Initiate UI

---
+++ Assets/Scripts/ScenesNavigation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ScenesNavigation : MonoBehaviour {
+
+	public void Goto_SignalFlow()
+    {
+        SceneManager.LoadScene("Build Signal Flow Graph");
+    }
+
+    public void Goto_BlockDiagram()
+    {
+
+    }
+}
